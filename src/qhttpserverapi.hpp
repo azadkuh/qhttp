6290c28103dff2fe5463e3a23dc5c31a72104ed5
Amir Zamani <azadkuh@live.com>
2014-06-21 18:58:04 +0430

    add whitespace chars to #ifdef lines.

--- src/qhttpserverapi.hpp
+++ src/qhttpserverapi.hpp
@@ -27,31 +27,34 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
-#ifdef Q_OS_WIN
+#   ifdef Q_OS_WIN
 // Define to export or import depending if we are building or using the library.
 // QHTTPSERVER_EXPORT should only be defined when building.
-#if defined(QHTTPSERVER_EXPORT)
-#define QHTTPSERVER_API __declspec(dllexport)
-#else
-#define QHTTPSERVER_API __declspec(dllimport)
-#endif
-#else
+#       if defined(QHTTPSERVER_EXPORT)
+#           define QHTTPSERVER_API __declspec(dllexport)
+#       else
+#           define QHTTPSERVER_API __declspec(dllimport)
+#       endif
+#   else
 // Define empty for other platforms
-#define QHTTPSERVER_API
-#endif
+#   define QHTTPSERVER_API
+#   endif
+
 #else
-#ifdef Q_WS_WIN
+
+#   ifdef Q_WS_WIN
 // Define to export or import depending if we are building or using the library.
 // QHTTPSERVER_EXPORT should only be defined when building.
-#if defined(QHTTPSERVER_EXPORT)
-#define QHTTPSERVER_API __declspec(dllexport)
-#else
-#define QHTTPSERVER_API __declspec(dllimport)
-#endif
-#else
+#       if defined(QHTTPSERVER_EXPORT)
+#           define QHTTPSERVER_API __declspec(dllexport)
+#       else
+#           define QHTTPSERVER_API __declspec(dllimport)
+#       endif
+#   else
 // Define empty for other platforms
-#define QHTTPSERVER_API
-#endif
+#       define QHTTPSERVER_API
+#   endif
+
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
