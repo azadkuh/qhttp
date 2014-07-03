84c8db6a051425433cd480e001df34d6e0ec5c31
Amir Zamani <azadkuh@live.com>
2014-07-03 13:32:40 +0430

    use QHttpResponse::end instead of write in basic-server.

--- example/basic-server/httpserver.cpp
+++ example/basic-server/httpserver.cpp
@@ -46,36 +46,36 @@ ClientConnection::processRequest(qhttp::server::QHttpRequest* req,
         ibody.append(chunk);
     });
 
-    QObject::connect(req, &qhttp::server::QHttpRequest::end, [this, req](){
+    QObject::connect(req, &qhttp::server::QHttpRequest::end, [this, req, res](){
+        printf("a new request (#%d) is comming from %s:%d\nurl: %s\n",
+               iconnectionId,
+               req->remoteAddress().toUtf8().constData(),
+               req->remotePort(),
+               qPrintable(req->url().toString())
+               );
+
         if ( req->method() == qhttp::EHTTP_POST )
             printf("body: \"%s\"\n", ibody.constData());
 
+        QString body = QString("Hello World\n    packet count = %1\n    time = %2\n")
+                       .arg(iconnectionId)
+                       .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
 
-        printf("end of client connection\n");
+        res->setHeader("content-length", QString::number(body.size()).toLatin1());
 
-        const qhttp::THeaderHash &headers = req->headers();
+        if ( req->headers().value("connection", "close") == "keep-alive" )
+            res->setHeader("connection", "keep-alive");
+        else
+            res->setHeader("connection", "close");
 
-        if ( headers.value("command") == "quit" ) {
+        res->writeHead(qhttp::ESTATUS_OK);
+        res->end(body.toUtf8());
+
+        if ( req->headers().value("command") == "quit" ) {
             printf("a quit has been requested!\n");
             emit requestQuit();
         }
     });
-
-    printf("a new request (#%d) is comming from %s:%d\nurl: %s\n",
-           iconnectionId,
-           req->remoteAddress().toUtf8().constData(),
-           req->remotePort(),
-           qPrintable(req->url().toString())
-           );
-
-    QString body = QString("Hello World\n    packet count = %1\n    time = %2\n")
-                   .arg(iconnectionId)
-                   .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"));
-
-    res->setHeader("content-length", QString::number(body.size()).toLatin1());
-    res->setHeader("connection", "close");
-    res->writeHead(qhttp::ESTATUS_OK);
-    res->write(body.toUtf8());
 }
 
 
