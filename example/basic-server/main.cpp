4bbf32ef39de7937c5d37223e360cff9cdf162bb
Amir Zamani <azadkuh@live.com>
2014-07-22 16:27:08 +0430

    add server handler.

--- example/basic-server/main.cpp
+++ example/basic-server/main.cpp
@@ -15,14 +15,15 @@ class ClientHandler : public QObject
 {
 public:
     explicit        ClientHandler(quint64 id, QHttpRequest* req, QHttpResponse* res)
-        : QObject(res), iconnectionId(id) {
+        : QObject(req), iconnectionId(id) {
         // append chunks of data into uniform body.
         req->onData([this](const QByteArray& chunk) {
             ibody.append(chunk);
         });
 
         // when all the incoming data are gathered, send some response to client.
-        req->onEnd([this, req, res](){
+        req->onEnd([this, req, res]() {
+
             printf("connection (#%llu ): request from %s:%d\nurl: %s\n",
                    iconnectionId,
                    req->remoteAddress().toUtf8().constData(),
@@ -52,7 +53,7 @@ public:
     }
 
     virtual        ~ClientHandler() {
-        puts("~ClientHandler(): I'm called automatically!");
+        puts("~ClientHandler(): I've being called automatically!");
     }
 
 protected:
@@ -69,19 +70,20 @@ int main(int argc, char ** argv) {
     quint64 iconnectionCounter = 0;
 
     QHttpServer server(&app);
-    server.listen(8080);
-    QObject::connect(&server,    &QHttpServer::newRequest,
-                     [&](QHttpRequest* req, QHttpResponse* res){
-        new ClientHandler(iconnectionCounter++,
-                          req,
-                          res);
+    server.listen(QHostAddress::Any, 8080, [&](QHttpRequest* req, QHttpResponse* res){
+        new ClientHandler(iconnectionCounter++, req, res);
         // this ClientHandler object will be deleted automatically when:
         // socket disconnects (automatically after data has been sent to the client)
         //     -> QHttpConnection destroys
-        //         -> QHttpRequest destroys
-        //         -> QHttpResponse destroys ->  ClientHandler destroys
+        //         -> QHttpRequest destroys -> ClientHandler destroys
+        //         -> QHttpResponse destroys
         // all by parent-child model of QObject.
     });
+
+    if ( !server.isListening() ) {
+        fprintf(stderr, "can not listen @8080!\n");
+        return -1;
+    }
 
     app.exec();
 }
